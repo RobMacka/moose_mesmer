21b8648846cc8b7b8d8567fc349416a10fbcc01f
RobMacka <73183527+RobMacka@users.noreply.github.com>
2025-05-30 00:50:38 +0100

    Add files via upload
    Header files for the calculation of the distribution of the scalar magnetic potential according to the magnetisation.

---
+++ modules/electromagnetics/include/kernels/ADDemagMKernel.h
@@ -0,0 +1,26 @@
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "ADKernel.h"
+
+class ADDemagMKernel : public ADKernel
+{
+public:
+  static InputParameters validParams();
+
+  ADDemagMKernel(const InputParameters & parameters);
+
+protected:
+  virtual ADReal computeQpResidual() override;
+
+  Real _coefficient;
+
+  const ADVectorVariableValue & _coupled_val;
+};
